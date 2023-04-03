f8e78d0ad709ec4dc38a45145f23ba3ab608f39b
dwinters <dwinters@mitre.org>
2023-04-03 12:27:35 -0400

    Preparing for distribution via npm.

--- util/packager.js
+++ util/packager.js
@@ -1,18 +1,19 @@
 import { readdirSync, readFileSync, writeFileSync, existsSync, mkdirSync } from 'fs';
+import { sep, join } from 'path';
 
 // Loop over the translated CQL ELM JSON files, encode them as base64, and 
 // overwrite the content strings in the corresponding Library resources.
 // At the same time, save the unencoded ELM JSON off into .js files so it is 
 // easier for the dashboard to import them.
 const inputPath = 'cql';
-const fshPath = 'fsh-tank/input/fsh';
-const fhirPath = 'fsh-tank/fsh-generated/resources'
-let exportPath = inputPath + '/export';
-if (!existsSync(exportPath)) mkdirSync(exportPath);
+const fshPath = join('fsh-tank','input','fsh');
+const fhirPath = join('fsh-tank','fsh-generated','resources');
+let exportBase = join('dist','cql');
+if (!existsSync(exportBase)) mkdirSync(exportBase, {recursive: true});
 let files = readdirSync(inputPath);
 for (const file of files) {
   if (/(\.json)$/.test(file)) {
-    const contentString = readFileSync(inputPath + '/' + file);
+    const contentString = readFileSync(inputPath + sep + file);
     // Create a .js version of the ELM .json file
     try {
       const varName = file.slice(0,-5).replace(/-/g,'v').replace(/\./g,'');
@@ -21,7 +22,7 @@ for (const file of files) {
         ' = ' +
         contentString;
       const jsFile = file.replace('.json','.mjs');
-      writeFileSync(inputPath + '/export/' + jsFile, updatedContents);
+      writeFileSync(join(exportBase,jsFile), updatedContents);
     } catch(e) {
       continue;
     }
@@ -29,24 +30,24 @@ for (const file of files) {
     const contentBase64 = Buffer.from(contentString).toString('base64');
     const fshFile = file.replace('.json','.fsh');
     try {
-      const outputContents = readFileSync(fshPath + '/' + fshFile).toString();
+      const outputContents = readFileSync(fshPath + sep + fshFile).toString();
       const updatedContents = outputContents.replace(
         /\* content\[\=\]\.data \= \".+\"/,
         '* content[=].data = \"' + contentBase64 + '\"'
       );
-      writeFileSync(fshPath + '/' + fshFile, updatedContents);
+      writeFileSync(fshPath + sep + fshFile, updatedContents);
     } catch(e) {
       continue;
     }
     // Replace the base64 string in the corresponding library .json file
     const jsonFile = 'Library-' + file;
     try {
-      const outputContents = readFileSync(fhirPath + '/' + jsonFile).toString();
+      const outputContents = readFileSync(fhirPath + sep + jsonFile).toString();
       const updatedContents = outputContents.replace(
         /\"data\": \".+\"/,
         '\"data\": \"' + contentBase64 + '\"'
       );
-      writeFileSync(fhirPath + '/' + jsonFile, updatedContents);
+      writeFileSync(fhirPath + sep + jsonFile, updatedContents);
     } catch(e) {
       continue;
     }
@@ -55,20 +56,25 @@ for (const file of files) {
 
 // Loop over all generated FHIR resources and convert them to js files.
 files = readdirSync(fhirPath);
-exportPath = fhirPath + '/export';
-if (!existsSync(exportPath)) mkdirSync(exportPath);
+exportBase = join('dist','fhir');
+if (!existsSync(exportBase)) mkdirSync(exportBase, {recursive: true});
 for (const file of files) {
   if (/(\.json)$/.test(file)) {
-    const contentString = readFileSync(fhirPath + '/' + file);
+    const contentString = readFileSync(fhirPath + sep + file);
     // Create a .js version of the ELM .json file
     try {
+      // Organize files by their resource type
+      const resource = JSON.parse(contentString);
+      const resourceType = resource.resourceType;
+      const exportPath = join(exportBase,resourceType);
+      if (!existsSync(exportPath)) mkdirSync(exportPath, {recursive: true});
       const varName = file.slice(0,-5).split('-')[1];
       const updatedContents = 'export const ' + 
         varName + 
         ' = ' +
         contentString;
       const jsFile = varName + '.js';
-      writeFileSync(fhirPath + '/export/' + jsFile, updatedContents);
+      writeFileSync(join(exportPath,jsFile), updatedContents);
     } catch(e) {
       continue;
     }
---
+++ util/resourceConverter.js
@@ -0,0 +1,299 @@
+import { ScreeningAndManagementTestType } from "./ValueSet/ScreeningAndManagementTestType";
+import { ScreeningAndManagementHistoryQuestionnaire } from "./Questionnaire/ScreeningAndManagementHistoryQuestionnaire";
+import { PertinentProcedureShortList } from "./ValueSet/PertinentProcedureShortList";
+import { PertinentConditionShortList } from "./ValueSet/PertinentConditionShortList";
+import { PertinentObservationShortList } from "./ValueSet/PertinentObservationShortList";
+import { QualifierValuesShortList } from "./ValueSet/QualifierValuesShortList";
+import { PertinentVaccinationShortList } from "./ValueSet/PertinentVaccinationShortList";
+
+export function resourceConverter(questionnaireResponse, patientReference, getIncrementalId) {
+
+  let resources = [];
+
+  const linkIds = questionnaireResponse.item.map(itm => itm.linkId);
+
+  if (linkIds.includes('test-date')) {
+
+    const testDate = questionnaireResponse.item
+      .filter(itm => itm.linkId === 'test-date')
+      .flatMap(itm => itm.answer)
+      [0].valueDate;
+
+    let existingId = questionnaireResponse.item
+      .filter(itm => itm.linkId === 'diagnostic-report-to-amend')
+      .flatMap(itm => itm.answer);
+    
+    if (existingId.length > 0) existingId = existingId[0]?.valueString;
+    else existingId = null;
+
+    let codeConclusionPair = [];
+    const testTypes = questionnaireResponse.item.filter(itm => itm.linkId === 'test-type').map(itm => itm.answer)[0];
+    testTypes.forEach(tt => {
+
+      const coding = tt.valueCoding;
+
+      const shortHandDisplay = ScreeningAndManagementTestType.compose.include[0].concept
+        .filter(cpt => cpt.code === coding.code)
+        .map(cpt => cpt.display)[0];
+      
+      const linkId = ScreeningAndManagementHistoryQuestionnaire.item
+        .filter(itm => itm?.enableWhen?.filter(ew => ew?.answerString === shortHandDisplay)?.length > 0)
+        .map(itm => itm?.linkId)[0];
+
+      const conclusion = questionnaireResponse.item
+        .filter(itm => itm.linkId === linkId)
+        .flatMap(itm => itm.answer)[0].valueCoding;
+      
+      codeConclusionPair.push({code: coding, conclusion: conclusion});
+
+    });
+
+    codeConclusionPair.forEach(ccp => {
+      let id = getIncrementalId();
+      resources.push({
+        resourceType: 'DiagnosticReport',
+        id: existingId ?? id,
+        identifier: [
+          {
+            use: ["secondary"],
+            system: "http://OUR-PLACEHOLDER-URL.com",
+            value: id
+          }
+        ],
+        status: "amended",
+        subject: {
+          reference: patientReference
+        },
+        effectiveDateTime: testDate + 'T00:00:00.000Z',
+        code: {
+          coding: [
+            ccp.code
+          ]
+        },
+        conclusionCode: [{
+          coding: [ccp.conclusion]
+        }]
+      })
+    });
+  
+  } else if (linkIds.includes('procedure-date')) {
+
+    const procedureDate = questionnaireResponse.item
+      .filter(itm => itm.linkId === 'procedure-date')
+      .flatMap(itm => itm.answer)
+      [0].valueDate;
+
+    let coding = questionnaireResponse.item
+      .filter(itm => itm.linkId === 'procedure-type')
+      .flatMap(itm => itm.answer)
+      [0].valueCoding;
+
+    const display = PertinentProcedureShortList.compose.include[0].concept
+      .filter(cpt => cpt.code === coding.code)
+      .map(cpt => cpt.designation[0].value)[0];
+
+    coding.display = display;
+
+    let existingId = questionnaireResponse.item
+      .filter(itm => itm.linkId === 'procedure-to-amend')
+      .flatMap(itm => itm.answer);
+    
+    if (existingId.length > 0) existingId = existingId[0]?.valueString;
+    else existingId = null;
+    let id = getIncrementalId();
+
+    resources.push({
+      resourceType: 'Procedure',
+      id: existingId ?? id,
+      identifier: [
+        {
+          use: ["secondary"],
+          system: "http://OUR-PLACEHOLDER-URL.com",
+          value: id
+        }
+      ],
+      status: "completed",
+      subject: {
+        reference: patientReference
+      },
+      performedDateTime: procedureDate + 'T00:00:00.000Z',
+      code: {
+        coding: [coding],
+        text: coding.display
+      }
+    });
+
+  } else if (linkIds.includes('condition-date')) {
+
+    const conditionDate = questionnaireResponse.item
+      .filter(itm => itm.linkId === 'condition-date')
+      .flatMap(itm => itm.answer)
+      [0].valueDate;
+
+    let coding = questionnaireResponse.item
+      .filter(itm => itm.linkId === 'condition-type')
+      .flatMap(itm => itm.answer)
+      [0].valueCoding;
+
+    const concepts = [
+      PertinentConditionShortList.compose.include[0].concept,
+      PertinentConditionShortList.compose.include[1].concept
+    ].flat();
+
+    const display = concepts
+      .filter(cpt => cpt.code === coding.code)
+      .map(cpt => cpt.designation[0].value)[0];
+
+    coding.display = display;
+
+    let existingId = questionnaireResponse.item
+      .filter(itm => itm.linkId === 'condition-to-amend')
+      .flatMap(itm => itm.answer);
+    
+    if (existingId.length > 0) existingId = existingId[0]?.valueString;
+    else existingId = null;
+    let id = getIncrementalId();
+
+    resources.push({
+      resourceType: 'Condition',
+      id: existingId ?? id,
+      identifier: [
+        {
+          use: ["secondary"],
+          system: "http://OUR-PLACEHOLDER-URL.com",
+          value: id
+        }
+      ],
+      clinicalStatus: {
+        coding: [{
+          code: "active",
+          system: "http://terminology.hl7.org/CodeSystem/condition-clinical"
+        }]
+      },
+      subject: {
+        reference: patientReference
+      },
+      onsetDateTime: conditionDate + 'T00:00:00.000Z',
+      code: {
+        coding: [coding],
+        text: coding.display
+      }
+    });
+
+  } else if (linkIds.includes('observation-date')) {
+
+    const observationDate = questionnaireResponse.item
+      .filter(itm => itm.linkId === 'observation-date')
+      .flatMap(itm => itm.answer)
+      [0].valueDate;
+
+    let coding = questionnaireResponse.item
+      .filter(itm => itm.linkId === 'observation-type')
+      .flatMap(itm => itm.answer)
+      [0].valueCoding;
+
+    const display = PertinentObservationShortList.compose.include[0].concept
+      .filter(cpt => cpt.code === coding.code)
+      .map(cpt => cpt.designation[0].value)[0];
+
+    let valueCoding = questionnaireResponse.item
+      .filter(itm => itm.linkId === 'observation-value')
+      .flatMap(itm => itm.answer)
+      [0].valueCoding;
+
+    const concepts = QualifierValuesShortList.compose.include[0].concept;
+
+    const valueDisplay = concepts
+      .filter(cpt => cpt.code === valueCoding.code)
+      .map(cpt => cpt.designation[0].value)[0];
+
+    valueCoding.display = valueDisplay;
+
+    coding.display = display;
+
+    let existingId = questionnaireResponse.item
+      .filter(itm => itm.linkId === 'observation-to-amend')
+      .flatMap(itm => itm.answer);
+    
+    if (existingId.length > 0) existingId = existingId[0]?.valueString;
+    else existingId = null;
+    let id = getIncrementalId();
+
+    resources.push({
+      resourceType: 'Observation',
+      id: existingId ?? id,
+      identifier: [
+        {
+          use: ["secondary"],
+          system: "http://OUR-PLACEHOLDER-URL.com",
+          value: id
+        }
+      ],
+      status: "final",
+      subject: {
+        reference: patientReference
+      },
+      effectiveDateTime: observationDate + 'T00:00:00.000Z',
+      code: {
+        coding: [coding],
+        text: coding.display
+      },
+      valueCodeableConcept: {
+        coding: [valueCoding],
+        text: valueCoding.display
+      },
+    });
+
+  } else if (linkIds.includes('immunization-date')) {
+
+    const immunizationDate = questionnaireResponse.item
+      .filter(itm => itm.linkId === 'immunization-date')
+      .flatMap(itm => itm.answer)
+      [0].valueDate;
+
+    let coding = questionnaireResponse.item
+      .filter(itm => itm.linkId === 'immunization-type')
+      .flatMap(itm => itm.answer)
+      [0].valueCoding;
+
+    const display = PertinentVaccinationShortList.compose.include[0].concept
+      .filter(cpt => cpt.code === coding.code)
+      .map(cpt => cpt.designation[0].value)[0];
+
+    coding.display = display;
+
+    let existingId = questionnaireResponse.item
+      .filter(itm => itm.linkId === 'immunization-to-amend')
+      .flatMap(itm => itm.answer);
+    
+    if (existingId.length > 0) existingId = existingId[0]?.valueString;
+    else existingId = null;
+    let id = getIncrementalId();
+
+    resources.push({
+      resourceType: 'Immunization',
+      id: existingId ?? id,
+      identifier: [
+        {
+          use: ["secondary"],
+          system: "http://OUR-PLACEHOLDER-URL.com",
+          value: id
+        }
+      ],
+      status: "completed",
+      subject: {
+        reference: patientReference
+      },
+      occurrenceDateTime: immunizationDate + 'T00:00:00.000Z',
+      vaccineCode: {
+        coding: [coding],
+        text: coding.display
+      }
+    });
+
+  }
+
+  return resources;
+
+}
+
